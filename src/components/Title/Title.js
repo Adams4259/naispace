951ef8a412aaaa9454d5f95c7f000dc9fa9af775
Adams4259 <adamsechwa18@gmail.com>
2023-02-06 09:35:58 +0300

    header component built

---
+++ src/components/Title/Title.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Title.css'
+
+
+function Title() {
+    return(
+        <div className="Title">
+            <h2>NAPOPS</h2>
+            <div className="Title-Subtitle">Nairobi Privately Owned Public Spaces</div>
+        </div>
+    )
+}
+
+export default Title;
